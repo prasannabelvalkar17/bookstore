854b6a5db005c90e1dba45515727350988b3cdb3
Prasanna <prasanna.belvalkar17@gmail.com>
2022-01-07 20:03:22 +0530

    Added csv reading writing functionality

--- src/main/java/com/books/bookstore/service/BookService.java
+++ src/main/java/com/books/bookstore/service/BookService.java
@@ -1,7 +1,15 @@
 package com.books.bookstore.service;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,9 +17,16 @@ import org.springframework.stereotype.Service;
 import com.books.bookstore.dto.BookDTO;
 import com.books.bookstore.entity.Book;
 import com.books.bookstore.repo.BookRepository;
+import com.opencsv.CSVReader;
+
 
 @Service
 public class BookService {
+	
+	private static final Logger log = LoggerFactory.getLogger(BookService.class);
+
+	private Integer booksSold = 0;
+	private Double totalAmount = Double.valueOf("0");
 
 	@Autowired
 	private BookRepository repo;
@@ -21,25 +36,38 @@ public class BookService {
 		if(book != null) {
 			return "There are "+book.getQuantity()+ " copies of "+ book.getIsbn()+ ". Price: "+book.getPrice()+ " each.";
 		}
-		return "No book found with isbn : "+isbn;
+		return "No book found with isbn : "+isbn+".";
 	}
 	
 	@Transactional
 	public String buy(String isbn, Integer quantity) {
-		Book book = repo.retriveBooks(isbn);
-		if(book.getQuantity() >= quantity) {
-			book.setQuantity(book.getQuantity()- quantity);
-			return "Sold "+quantity+ " copies of "+book.getIsbn()+ " at "+book.getPrice()+ " each. Total: "+quantity*book.getPrice();
-		}else {
-			return "Less quantity available";
+		try {
+			Book book = repo.retriveBooks(isbn);
+			if(book.getQuantity() >= quantity) {
+				book.setQuantity(book.getQuantity()- quantity);
+				this.booksSold = this.booksSold + quantity;
+				this.totalAmount = this.totalAmount+ (quantity*book.getPrice());
+				log.info("Sold {} copies of {} at {} each. Total: {}.",quantity, book.getIsbn(), book.getPrice(), String.format("%.2f", quantity*book.getPrice()));
+				return "Sold "+quantity+ " copies of "+book.getIsbn()+ " at "+book.getPrice()+ " each. Total: "+ String.format("%.2f", quantity*book.getPrice())+".";
+			}else {
+				log.info("Insufficient quantity of {}.", isbn);
+				return "Insufficient quantity of "+isbn+ ".";
+			}
+		}catch(Exception e) {
+			log.error("Book {} not available.", isbn);
+			return "Book "+isbn+ " not available.";
 		}
+		
 	}
 	
 	@Transactional
 	public String returnBook(String isbn, Integer quantity) {
 		Book book = repo.retriveBooks(isbn);
 		book.setQuantity(book.getQuantity() + quantity);
-		return "Refund "+quantity+ " copies of "+book.getIsbn()+ " at "+book.getPrice()+ " each. Total: -"+quantity*book.getPrice();
+		this.booksSold = this.booksSold - quantity;
+		this.totalAmount = this.totalAmount - (quantity*book.getPrice());
+		log.info("Refund {} copies of {} at {} each. Total: -{}", quantity, book.getIsbn(), book.getPrice(), String.format("%.2f", quantity*book.getPrice()));
+		return "Refund "+quantity+ " copies of "+book.getIsbn()+ " at "+book.getPrice()+ " each. Total: -"+ String.format("%.2f", quantity*book.getPrice())+".";
 	}
 	
 	public String receiveNewBook(BookDTO bookDto) {
@@ -47,10 +75,11 @@ public class BookService {
 			Book book = new Book();
 			BeanUtils.copyProperties(bookDto, book);
 			repo.save(book);
-			return "Received "+book.getQuantity()+ " copies of book "+book.getIsbn();
+			log.info("Received {} copies of book {}.", book.getQuantity(), book.getIsbn());
+			return "Received "+book.getQuantity()+ " copies of book "+book.getIsbn()+".";
 		}catch(Exception e) {
 			e.printStackTrace();
-			return "Error occured while receiving book" + e.getMessage();
+			return "Error occured while receiving book" + e.getMessage()+".";
 		}
 	}
 	
@@ -58,6 +87,88 @@ public class BookService {
 	public String updateBookPrice(BookDTO bookDto) {
 		Book book = repo.retriveBooks(bookDto.getIsbn());
 		book.setPrice(bookDto.getPrice());
+		log.info("New price for {}: {}", book.getIsbn(), book.getPrice());
 		return "New price for "+book.getIsbn()+ ": "+book.getPrice();
+	}
+	
+	@Transactional
+	public String processBatch() {
+		booksSold = 0;
+		totalAmount = Double.valueOf("0");
+		StringBuilder result = new StringBuilder();
+//		File csvOutputFile = new File("src/main/resources/ProcessedBatch.csv");
+		try (FileReader filereader = new FileReader("src/main/resources/batch.csv"); 
+				CSVReader csvReader = new CSVReader(filereader);
+//				PrintWriter pw = new PrintWriter(csvOutputFile)
+				FileWriter fw = new FileWriter("src/main/resources/ProcessedBatch.csv");
+				){
+	        
+	        String[] nextRecord;
+	 
+	        while ((nextRecord = csvReader.readNext()) != null) {
+	        	switch (nextRecord[0]) {
+				case "query":
+					result.append(retriveBooks(nextRecord[1]));
+					result.append("\n");
+					break;
+					
+				case "buy":
+					result.append(buy(nextRecord[1], Integer.valueOf(nextRecord[2])));
+					result.append("\n");
+					break;
+					
+				case "return":
+					result.append(returnBook(nextRecord[1], Integer.valueOf(nextRecord[2])));
+					result.append("\n");
+					break;
+					
+				case "receive":
+					BookDTO receivedBook = new BookDTO();
+					receivedBook.setIsbn(nextRecord[1]);
+					receivedBook.setPrice(Double.valueOf(nextRecord[3]));
+					receivedBook.setQuantity(Integer.valueOf(nextRecord[2]));
+					result.append(receiveNewBook(receivedBook));
+					result.append("\n");
+					break;
+					
+				case "update":
+					BookDTO updatedBook = new BookDTO();
+					updatedBook.setIsbn(nextRecord[1]);
+					updatedBook.setPrice(Double.valueOf(nextRecord[2]));
+					result.append(updateBookPrice(updatedBook));
+					result.append("\n");
+					break;
+					
+				case "multibuy":
+					for(int i = 1; i < nextRecord.length; i=i+2) {
+						result.append(buy(nextRecord[i], Integer.valueOf(nextRecord[i+1])));
+						result.append("\n");
+					}
+					
+					break;
+					
+				case "multireturn":
+					for(int i = 1; i < nextRecord.length; i=i+2) {
+						result.append(returnBook(nextRecord[i], Integer.valueOf(nextRecord[i+1])));
+						result.append("\n");
+					}
+					break;
+
+				default:
+					break;
+				}
+	        	
+	        }
+	        result.append("Total books sold: "+this.booksSold+"\n");
+	        result.append("Total amount: "+this.totalAmount+"\n");
+	        result.append("Inventory:\n");
+	        List<Book> bookList = repo.findAll();
+	        bookList.stream().filter(item -> !item.getIsbn().equals("test")).forEach(item -> result.append(item.getIsbn()+ ":"+item.getQuantity()+"\n"));
+	        fw.append(result);
+	    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+		return result.toString();
 	}
 }
